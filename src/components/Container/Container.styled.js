aae6665d613d4753c06053c69c1cb9501a674661
Vadim Dudkevich <justin_duda@ukr.net>
2022-06-04 20:56:58 +0300

    add fixs styles

--- src/components/Container/Container.styled.js
+++ src/components/Container/Container.styled.js
@@ -1,9 +1,16 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  max-width: 460px;
-  margin: 0 auto;
-  padding: 0 15px;
+  max-width: 350px;
+  margin: 20px auto;
+  padding: 20px;
+
+  color: #fff;
+
+  background-color: #2d2c2e;
+  border-radius: 10px;
+  box-shadow: 4px 4px 10px 0 rgb(37 38 50 / 70%);
+
   @media screen and (min-width: 768px) {
     max-width: 500px;
   }
@@ -13,6 +20,11 @@ export const Wrapper = styled.div`
 `;
 
 export const Title = styled.h2`
-  color: #2d2c2e;
-  margin-bottom: 20px;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  font-weight: 900;
 `;
